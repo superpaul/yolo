80ce1c2347dc2113425bebdb349072ad101a7f0a
Dan Brown <roooms@gmail.com>
2015-02-15 22:48:31 +0000

    Tidy up and fix OTP tags

--- ubuntu_install_kerl_erlang.sh
+++ ubuntu_install_kerl_erlang.sh
@@ -4,6 +4,7 @@ sudo apt-get update --fix-missing && sudo apt-get upgrade -y
 
 # install dependencies to build erlang from source
 sudo apt-get install -y \
+autoconf \
 build-essential \
 libncurses5-dev \
 openssl \
@@ -28,12 +29,12 @@ sudo curl -o /usr/local/bin/kerl https://raw.githubusercontent.com/spawngrid/ker
 sudo chown root:root /usr/local/bin/kerl
 sudo chmod a+x /usr/local/bin/kerl
 
-# install erlang R15B01
-kerl build git git://github.com/basho/otp.git OTP_R15B01 R15B01
+# install basho version of erlang R15B01
+kerl build git git://github.com/basho/otp.git basho_OTP_R15B01_scheduler_patch R15B01
 kerl install R15B01 ~/erlang/R15B01
 #. ~/erlang/R15B01/activate
 
 # install basho version of erlang R16B02
-kerl build git git://github.com/basho/otp.git OTP_R16B02_basho5 R16B02-basho5
-kerl install R16B02-basho5 ~/erlang/R16B02-basho5
-#. ~/erlang/R16B02-basho5/activate
+kerl build git git://github.com/basho/otp.git OTP_R16B02_basho7 R16B02_basho7
+kerl install R16B02_basho7 ~/erlang/R16B02_basho7
+#. ~/erlang/R16B02-basho7/activate
